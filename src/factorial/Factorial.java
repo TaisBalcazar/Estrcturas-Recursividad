95de723f655b6a228def0f5363e0d1ccd4139766
TaisBalcazar <92813903+TaisBalcazar@users.noreply.github.com>
2022-04-26 12:06:58 -0500

    Add files via upload

---
+++ src/factorial/Factorial.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package factorial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SALA H
+ */
+public class Factorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        System.out.println("Ingrese el valor factorial");
+        numero = entrada.nextInt();
+        System.out.printf("El resultado es : %d\n",factorial(numero));
+
+    }
+
+    public static int factorial(int numero) {
+        if (numero <= 1) {
+            return 1;
+        } else {
+            return numero * factorial(numero - 1);
+        }
+    }
+}
